d6343390cc2ac9382fbe0d9234ea720f834783a8
Jhonathan Ade <ade.jhonathan24@gmail.com>
2021-01-09 22:51:19 -0500

    commission form - added inputs and styled it a bit

--- react-app/src/Components/Forms/CommissionForm.js
+++ react-app/src/Components/Forms/CommissionForm.js
@@ -1,31 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+// CSS
+import "./CommissionForm.css"
 
 const CommissionForm = () => {
+  const [duration, setDuration] = useState(false)
+
   return (
-    <>
-    <form>
-      <div>
-        <label>
-          Title
-        </label>
-        <input
-          name="title"
-          type="text"
-          placeholder="Title"
-        />
-      </div>
-      <div>
-        <label>
-          Description
-        </label>
-        <input
-          name="description"
-          type="textarea"
-          placeholder="Add description"
-        />
-      </div>
-    </form>
-    </>
+    <div className="commform-div">
+      <form>
+          <label>
+            Title
+          </label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            />
+          <label>
+            Description
+          </label>
+          <textarea
+            name="description"
+            type="textarea"
+            placeholder="Add description"
+            />
+          <label>
+            Image:
+          </label>
+          <input
+            name="url"
+            type="text"
+            placeholder="upload an image" 
+            />
+          <label>
+            Price:
+          </label>
+          <input 
+            name="price"
+            type="number"
+            min= "0.00"
+            placeholder="$0.00"
+            />
+          <label>Number Of Requests</label>
+          {/* <p> Set the maximum amount of requests that you want to receive for this commission</p> */}
+          <input
+            name="requests"
+            type="number"
+            min="0"
+            placeholder="0"
+            />
+          <label>Duration</label>
+          <p>Do you want to set a duration for this commission?</p>
+        <div className="duration-options">
+          <label>
+            Yes
+          <input type="radio" value="true" onChange={setDuration}/>
+          </label>
+          <label>
+            No
+          <input type="radio" value="no" onChange={setDuration}/>
+          </label>
+        </div>
+          <label>
+            Ends:
+          </label>
+          <input 
+            name="date"
+            type="date"
+            />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
