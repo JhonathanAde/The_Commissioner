401beb24d1cf9aeb8b3681ab5584d336fd2b033f
Jhonathan Ade <ade.jhonathan24@gmail.com>
2021-06-30 21:58:50 -0400

    added new seed data for ratings table

--- app/seeds/ratings.py
+++ app/seeds/ratings.py
@@ -2,16 +2,92 @@ from app.Models import db, Rating
 
 def seed_ratings():
 
+  # DEMO
   demo_rating1 = Rating(
-    rating = None,
+    rating = 4,
     comment = "This is an awesome work of art! Keep it up!",
     user_id = 4,
     artist_id = 1,
     commission_id = 1
   )
 
+  demo_rating2 = Rating (
+    rating = 4,
+    comment = "Yo, the work I commissioned from this artist was amazing! I would definitely get work from him again",
+    user_id = 6,
+    artist_id = 1,
+    commission_id = 1
+  )
+
+  demo_rating3 = Rating (
+    rating = 3,
+    comment = "Great work, but the drawing came out a little smaller than I expected..",
+    user_id = 7,
+    artist_id = 1,
+    commission_id = 1
+  )
+
+  demo_rating4 = Rating (
+    rating = 5,
+    comment = "Absolutely fantastic! Definitely one of my most favorite commissioners out there!",
+    user_id = 3,
+    artist_id = 1,
+    commission_id = 1
+  )
+
+  # James Jean
+  JamesJean_rating1 = Rating (
+    rating = 5,
+    comment = "Everything about James Jean’s work is amazing! Im absolutely honored to have one of his works, now at my office. ",
+    user_id = 8,
+    artist_id = 2,
+    commission_id = 4
+  )
+
+  JamesJean_rating2 = Rating (
+    rating = 4,
+    comment = "I absolutely loved getting a commission done by James Jean. He is an absolute beast at his work and would definitely recommend his work to everyone seeking an amazing commission.",
+    user_id = 3,
+    artist_id = 2,
+    commission_id = 4
+  )
+
+  JamesJean_rating3 = Rating (
+    rating = 4,
+    comment = "The colors! I really love James Jean’s color choices. Must commission from him again!",
+    user_id = 4,
+    artist_id = 2,
+    commission_id = 4
+  )
+
+  JamesJean_rating4 = Rating (
+    rating = 2,
+    comment = "Wasn’t really a fan of how abstract the commission was. I just wanted a regular picture of Batman… I’m just a hater.",
+    user_id = 7,
+    artist_id = 2,
+    commission_id = 4
+  )
+
+
+  JamesJean_rating5 = Rating (
+    rating = 4,
+    comment = "As a soon to be mother I really loved how this commission came out!",
+    user_id = 5,
+    artist_id = 2,
+    commission_id = 5
+  )
+
+
 
   db.session.add(demo_rating1)
+  db.session.add(demo_rating2)
+  db.session.add(demo_rating3)
+  db.session.add(demo_rating4)
+  db.session.add(JamesJean_rating1)
+  db.session.add(JamesJean_rating2)
+  db.session.add(JamesJean_rating3)
+  db.session.add(JamesJean_rating4)
+  db.session.add(JamesJean_rating5)
 
   db.session.commit()
 
--- react-app/src/Components/Pages/Profile/Profilepage.js
+++ react-app/src/Components/Pages/Profile/Profilepage.js
@@ -17,7 +17,9 @@ import Footer from '../../Footer/Footer'
 
 const Profilepage = ({authenticated, user}) => {
 
-  var visitor 
+  var visitor
+  var buttons
+  var displayReqs = false;
  
   const [userRequests, setUserRequests] = useState(null)
   const [userCommissions, setUserCommissions] = useState(null)
@@ -25,6 +27,9 @@ const Profilepage = ({authenticated, user}) => {
   const [visitorCommission, setVisitorCommissions] = useState(null)
   const [otherUserName, setOtherUserName] = useState(null)
   const [otherLocation, setOtherLocation] = useState(null)
+  const [commButton, setCommButton] = useState(null)
+  const [reqButton, setReqButton] = useState(null)
+  const [requestInfo, setRequestInfo] = useState('')
   const {userId} = useParams()
   const {pathname} = useLocation();
   const history = useHistory();
@@ -46,10 +51,21 @@ const Profilepage = ({authenticated, user}) => {
 
   useEffect(() => {
     (async () => {
-       const request = await getRequestsById(user.id)
+        const commButtonEl = document.getElementById('profile-commissions__button-active')
+        const reqButtonEl = document.getElementById('profile-requests__button')
+        const request = await getRequestsById(user.id)
         const commissions = await getCommissionsById(user.id)
         setUserRequests(request)
         setUserCommissions(commissions)
+        setCommButton(commButtonEl);
+        setReqButton(reqButtonEl);
+    })()
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      const requests = await getRequestsById(user.id)
+      setRequestInfo(requests)
     })()
   }, [])
 
@@ -65,6 +81,40 @@ const Profilepage = ({authenticated, user}) => {
 
   checkLocation();
 
+  console.log(commButton)
+  console.log(reqButton)
+  console.log(requestInfo);
+
+  const reqClickHandler = () => {
+      buttons = 2;
+      reqButton.id = "profile-requests__button-active";
+      displayReqs = true;
+      if(buttons !== 1){
+      commButton.id = "profile-commissions__button";
+      // window.location.reload()
+    }
+  }
+
+  const commClickHandler = () => {
+    buttons = 1;
+    commButton.id = "profile-commissions__button-active";
+    displayReqs = false;
+    if(buttons !== 2){
+      reqButton.id = "profile-requests__button"
+    }
+  }
+
+  const checkButtons = () => {
+    if(buttons !== 1){
+      commButton.id = "profile-commissions__button";
+    }
+    else if(buttons !== 2){
+      reqButton.id = "profile-requests__button"
+    }
+
+  }
+
+  // checkButtons();
 
   return (
     <>
@@ -91,16 +141,17 @@ const Profilepage = ({authenticated, user}) => {
         </div>
         <div className="profilepage-display profile-content">
           <div className="filler-1"></div>
-          <div>
-          <button>Commissions</button>
-          <button>Requests</button>
+          <div id="profile-display__buttons">
+          <button id="profile-commissions__button-active" onClick={commClickHandler}>Commissions</button>
+          <button id="profile-requests__button"  onClick={reqClickHandler}>Requests</button>
           </div>
           <div className="profilepage-display profile-content__divider"></div>
           <div className="profilepage-display profile-content__display">
-            {
+            { 
               userCommissions && userCommissions.commissions.map((comms, idx) => (
                 <CommissionCards comms={comms} key={idx}/>
-              ))}
+              ))
+            }
           </div>
         </div>
       </div>
